68af916e06fd8350aedd49dcb72fd30168a96635
Magician <pranshuchawla19@gmail.com>
2025-02-20 21:07:19 -0500

    css for posts + functioning blogging sysem

--- blog/src/app/posts/[id]/page.js
+++ blog/src/app/posts/[id]/page.js
@@ -1,15 +1,16 @@
 import { getAllPostIds, getPostData } from '@/lib/posts';
-
+import styles from './page.module.css';
 export async function generateStaticParams() {
   const paths = getAllPostIds();
   return paths.map((path) => path.params);
 }
 
 export default async function Post({ params }) {
-  const postData = await getPostData(params.id);
+    const resolvedParams = await params;
+    const postData = await getPostData(resolvedParams.id);
 
   return (
-    <div>
+    <div className={styles.post}>
       <h1>{postData.title}</h1>
       <p>{postData.date}</p>
       <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
---
+++ blog/src/posts/basicTerminology.md
@@ -0,0 +1,5 @@
+---
+title: "Basic Full Stack Terminology" 
+date: "02-19-2025"
+---
+Let's start with
